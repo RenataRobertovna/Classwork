dd7401cb451c401fb5f3da2a42c1159580423214
Renata <safaryonok@gmail.com>
2020-06-05 21:39:16 +0300

    Второй урок
    Lesson 2

---
+++ cmake-build-debug/CMakeFiles/22_11_2019.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "22_11_2019.exe"
+  "22_11_2019.exe.manifest"
+  "22_11_2019.pdb"
+  "CMakeFiles/22_11_2019.dir/main.cpp.obj"
+  "lib22_11_2019.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/22_11_2019.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int magicNumber = 153;
+    cout<<"===================================================\n\n";
+    cout<<"         My magic number between 1 and 500         \n\n";
+    cout<<"===================================================\n\n";
+
+    int user = 0;
+    for (int n=1; n<=500;
+}
